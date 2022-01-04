9aeafc8a37659821e61d78787b8469e4e070db66
Darko Malogajski <darko.malogajski@gmail.com>
2022-01-04 18:56:41 +0100

    change: main menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -22,38 +22,38 @@
     <div class="-mx-1">
         <ul class="flex w-full flex-wrap items-center h-10">
 
-            <li class="block relative" x-data="{showChildren:false}" @click.away="showChildren=false">
-                <a href="#"
-                   class="flex items-center h-10 leading-10 px-4 rounded cursor-pointer no-underline hover:no-underline transition-colors duration-100 mx-1 bg-indigo-500 text-white"
-                   @click.prevent="showChildren=!showChildren">
-                    <span class="mr-3 text-xl"> <i class="mdi mdi-gauge"></i> </span>
-                    <span>Admin</span>
-                    <span class="ml-2"> <i class="mdi mdi-chevron-down"></i> </span>
-                </a>
-                <div class="bg-white shadow-md rounded border border-gray-300 text-sm absolute top-auto left-0 min-w-full w-56 z-30 mt-1" x-show="showChildren"
-                     style="display: none;" x-transition:enter="transition ease duration-300 transform" x-transition:enter-start="opacity-0 translate-y-2"
-                     x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease duration-300 transform"
-                     x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4">
-                    <span class="absolute top-0 left-0 w-3 h-3 bg-white border transform rotate-45 -mt-1 ml-6"></span>
-                    <div class="bg-white rounded w-full relative z-10 py-1">
-                        <ul class="list-reset">
-                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">
-                                <a href="#"
-                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span
-                                        class="flex-1">Dashboard 1</span> </a>
-                            </li>
-                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">
-                                <a href="#"
-                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span
-                                        class="flex-1">Dashboard 2</span> </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </li>
+{{--            <li class="block relative" x-data="{showChildren:false}" @click.away="showChildren=false">--}}
+{{--                <a href="#"--}}
+{{--                   class="flex items-center h-10 leading-10 px-4 rounded cursor-pointer no-underline hover:no-underline transition-colors duration-100 mx-1 bg-indigo-500 text-white"--}}
+{{--                   @click.prevent="showChildren=!showChildren">--}}
+{{--                    <span class="mr-3 text-xl"> <i class="mdi mdi-gauge"></i> </span>--}}
+{{--                    <span>Admin</span>--}}
+{{--                    <span class="ml-2"> <i class="mdi mdi-chevron-down"></i> </span>--}}
+{{--                </a>--}}
+{{--                <div class="bg-white shadow-md rounded border border-gray-300 text-sm absolute top-auto left-0 min-w-full w-56 z-30 mt-1" x-show="showChildren"--}}
+{{--                     style="display: none;" x-transition:enter="transition ease duration-300 transform" x-transition:enter-start="opacity-0 translate-y-2"--}}
+{{--                     x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease duration-300 transform"--}}
+{{--                     x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4">--}}
+{{--                    <span class="absolute top-0 left-0 w-3 h-3 bg-white border transform rotate-45 -mt-1 ml-6"></span>--}}
+{{--                    <div class="bg-white rounded w-full relative z-10 py-1">--}}
+{{--                        <ul class="list-reset">--}}
+{{--                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">--}}
+{{--                                <a href="#"--}}
+{{--                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span--}}
+{{--                                        class="flex-1">Dashboard 1</span> </a>--}}
+{{--                            </li>--}}
+{{--                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">--}}
+{{--                                <a href="#"--}}
+{{--                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span--}}
+{{--                                        class="flex-1">Dashboard 2</span> </a>--}}
+{{--                            </li>--}}
+{{--                        </ul>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </li>--}}
 
             <li class="block relative">
-                <a href="#"
+                <a href="{{ route('home') }}"
                    class="flex items-center h-10 leading-10 px-4 rounded cursor-pointer no-underline hover:no-underline transition-colors duration-100 mx-1 hover:bg-gray-100">
                     <span class="mr-3 text-xl"> <i class="mdi mdi-widgets-outline"></i> </span>
                     <span>Home</span>
@@ -234,11 +234,6 @@
                                    class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span
                                         class="flex-1">User Profile</span> </a>
                             </li>
-                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">
-                                <a href="#"
-                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span
-                                        class="flex-1">Account Settings</span> </a>
-                            </li>
 
 {{--                            Invoices--}}
                             <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">
@@ -308,45 +303,45 @@
             </li>
 
 
-            <li class="block relative" x-data="{showChildren:false}" @click.away="showChildren=false">
-                <a href="#"
-                   class="flex items-center h-10 leading-10 px-4 rounded cursor-pointer no-underline hover:no-underline transition-colors duration-100 mx-1 hover:bg-gray-100"
-                   @click.prevent="showChildren=!showChildren">
-                    <span class="mr-3 text-xl"> <i class="mdi mdi-apple-safari"></i> </span>
-                    <span>Apps</span>
-                    <span class="ml-2"> <i class="mdi mdi-chevron-down"></i> </span>
-                </a>
-                <div class="bg-white shadow-md rounded border border-gray-300 text-sm absolute top-auto left-0 min-w-full w-56 z-30 mt-1" x-show="showChildren"
-                     x-transition:enter="transition ease duration-300 transform" x-transition:enter-start="opacity-0 translate-y-2"
-                     x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease duration-300 transform"
-                     x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4" style="display: none;">
-                    <span class="absolute top-0 left-0 w-3 h-3 bg-white border transform rotate-45 -mt-1 ml-6"></span>
-                    <div class="bg-white rounded w-full relative z-10 py-1">
-                        <ul class="list-reset">
-                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">
-                                <a href="#"
-                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span
-                                        class="flex-1">Calender</span> </a>
-                            </li>
-                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">
-                                <a href="#"
-                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span
-                                        class="flex-1">Chat</span> </a>
-                            </li>
-                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">
-                                <a href="#"
-                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span
-                                        class="flex-1">Email</span> </a>
-                            </li>
-                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">
-                                <a href="#"
-                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span
-                                        class="flex-1">Todo</span> </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </li>
+{{--            <li class="block relative" x-data="{showChildren:false}" @click.away="showChildren=false">--}}
+{{--                <a href="#"--}}
+{{--                   class="flex items-center h-10 leading-10 px-4 rounded cursor-pointer no-underline hover:no-underline transition-colors duration-100 mx-1 hover:bg-gray-100"--}}
+{{--                   @click.prevent="showChildren=!showChildren">--}}
+{{--                    <span class="mr-3 text-xl"> <i class="mdi mdi-apple-safari"></i> </span>--}}
+{{--                    <span>Apps</span>--}}
+{{--                    <span class="ml-2"> <i class="mdi mdi-chevron-down"></i> </span>--}}
+{{--                </a>--}}
+{{--                <div class="bg-white shadow-md rounded border border-gray-300 text-sm absolute top-auto left-0 min-w-full w-56 z-30 mt-1" x-show="showChildren"--}}
+{{--                     x-transition:enter="transition ease duration-300 transform" x-transition:enter-start="opacity-0 translate-y-2"--}}
+{{--                     x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease duration-300 transform"--}}
+{{--                     x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4" style="display: none;">--}}
+{{--                    <span class="absolute top-0 left-0 w-3 h-3 bg-white border transform rotate-45 -mt-1 ml-6"></span>--}}
+{{--                    <div class="bg-white rounded w-full relative z-10 py-1">--}}
+{{--                        <ul class="list-reset">--}}
+{{--                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">--}}
+{{--                                <a href="#"--}}
+{{--                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span--}}
+{{--                                        class="flex-1">Calender</span> </a>--}}
+{{--                            </li>--}}
+{{--                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">--}}
+{{--                                <a href="#"--}}
+{{--                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span--}}
+{{--                                        class="flex-1">Chat</span> </a>--}}
+{{--                            </li>--}}
+{{--                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">--}}
+{{--                                <a href="#"--}}
+{{--                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span--}}
+{{--                                        class="flex-1">Email</span> </a>--}}
+{{--                            </li>--}}
+{{--                            <li class="relative" x-data="{showChildren:false}" @mouseleave="showChildren=false" @mouseenter="showChildren=true">--}}
+{{--                                <a href="#"--}}
+{{--                                   class="px-4 py-2 flex w-full items-start hover:bg-gray-100 no-underline hover:no-underline transition-colors duration-100 cursor-pointer"> <span--}}
+{{--                                        class="flex-1">Todo</span> </a>--}}
+{{--                            </li>--}}
+{{--                        </ul>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </li>--}}
 
 
 {{--            Authentication Links --}}
